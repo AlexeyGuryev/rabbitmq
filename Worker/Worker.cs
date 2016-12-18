db9b713c7fba4b5532fd9e79696f4ad1366cd5fc
Alexey Guryev <alguryew@gmail.com>
2016-12-18 10:22:42 +0300

    Separate examples: Hello and Worker

--- Worker/Worker.cs
+++ Worker/Worker.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Text;
+using System.Threading;
 using Client;
 using RabbitMQ.Client.Events;
 using RabbitMQ.Client;
 
-namespace Receive
+namespace Worker
 {
-    class Receive
+    class Worker
     {
+        private const string QUEUE_NAME = "task_queue";
+
         static void Main(string[] args)
         {
             using (var connection = Connector.CreateConnectionFactory().CreateConnection())
@@ -15,8 +18,8 @@ namespace Receive
                 using (var channel = connection.CreateModel())
                 {
                     channel.QueueDeclare(
-                        queue: "hello",
-                        durable: false,
+                        queue: QUEUE_NAME,
+                        durable: true,
                         exclusive: false,
                         autoDelete: false,
                         arguments: null);
@@ -28,8 +31,14 @@ namespace Receive
                         var body = eventArgs.Body;
                         var message = Encoding.UTF8.GetString(body);
                         Console.WriteLine("Message received: {0}", message);
+
+                        int dots = message.Split('.').Length - 1;
+                        Thread.Sleep(1000 * dots);
+                        Console.WriteLine("Done");
+
+                        channel.BasicAck(deliveryTag: eventArgs.DeliveryTag, multiple: false);
                     };
-                    channel.BasicConsume("hello", noAck: true, consumer: consumer);
+                    channel.BasicConsume(queue: QUEUE_NAME, noAck: false, consumer: consumer);
                     Console.WriteLine("Press any key");
                     Console.ReadLine();
                 }
