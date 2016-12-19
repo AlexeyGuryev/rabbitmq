d351a8970d4e9c4967ec1a44f0ccb53e61b8201f
Alexey Guryev <alguryew@gmail.com>
2016-12-19 08:56:17 +0300

    RPC example

---
+++ RPCClient/RPCClient.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core;
+using RabbitMQ.Client;
+
+namespace RPCClient
+{
+    public class RPCClient
+    {
+        private IConnection connection;
+        private IModel channel;
+        private string replyQueueName;
+        private QueueingBasicConsumer consumer;
+
+        public RPCClient()
+        {
+            connection = Connector.CreateConnectionFactory().CreateConnection();
+            channel = connection.CreateModel();
+            replyQueueName = channel.QueueDeclare().QueueName;
+            consumer = new QueueingBasicConsumer(channel);
+            channel.BasicConsume(queue: replyQueueName, noAck: true, consumer: consumer);
+        }
+
+        public string Call(string message)
+        {
+            var corrId = Guid.NewGuid().ToString();
+            var props = channel.CreateBasicProperties();
+            props.ReplyTo = replyQueueName;
+            props.CorrelationId = corrId;
+
+            var messageBytes = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish(exchange: "", routingKey: "rpc_queue", basicProperties: props, body: messageBytes);
+
+            while (true)
+            {
+                var eventArgs = consumer.Queue.Dequeue();
+                if (eventArgs.BasicProperties.CorrelationId == corrId)
+                {
+                    return Encoding.UTF8.GetString(eventArgs.Body);
+                }
+            }
+        }
+
+        public void Close()
+        {
+            connection.Close();
+        }
+    }
+
+
+    class RPC
+    {
+        static void Main()
+        {
+            var rpcClient = new RPCClient();
+            Console.WriteLine("Requesting fib(30)");
+            var response = rpcClient.Call("30");
+            Console.WriteLine("Got: {0}", response);
+            rpcClient.Close();
+        }
+    }
+}
---
+++ RPCServer/RPCServer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Text;
+using Core;
+using RabbitMQ.Client;
+
+namespace RPCServer
+{
+    class RPCServer
+    {
+        static void Main(string[] args)
+        {
+            using (var connection = Connector.CreateConnectionFactory().CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(
+                    queue: "rpc_queue",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                channel.BasicQos(0, 1, global: false);
+                var consumer = new QueueingBasicConsumer(channel);
+                channel.BasicConsume(queue: "rpc_queue", noAck: false, consumer: consumer);
+                Console.WriteLine("Awaiting RPC requests");
+
+                while (true)
+                {
+                    string response = string.Empty;
+                    var eventArgs = consumer.Queue.Dequeue();
+                    var body = eventArgs.Body;
+                    var props = eventArgs.BasicProperties;
+
+                    var replyProps = channel.CreateBasicProperties();
+                    replyProps.CorrelationId = props.CorrelationId;
+
+                    var message = Encoding.UTF8.GetString(body);
+                    int n = 0;
+                    if (int.TryParse(message, out n))
+                    {
+                        Console.WriteLine("[.] fib({0})", message);
+                        response = Fib(n).ToString();
+                    }
+
+                    var responseBytes = Encoding.UTF8.GetBytes(response);
+                    channel.BasicPublish(exchange: "", routingKey: props.ReplyTo, basicProperties: replyProps, body: responseBytes);
+                    channel.BasicAck(deliveryTag: eventArgs.DeliveryTag, multiple: false);
+                }
+            }
+        }
+
+        private static int Fib(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return n;
+            }
+
+            return Fib(n - 1) + Fib(n - 2);
+        }
+    }
+}
